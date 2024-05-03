ed2b08ab061e3837a42f08b7e1a6fcb31ba55049
Azri Othman <azri.othman@Azris-MacBook-Pro.local>
2024-05-03 23:28:48 +0800

    Add missing field in phonebook entry entity

--- domain/entities/phonebook-entry.js
+++ domain/entities/phonebook-entry.js
@@ -1,8 +1,10 @@
 class PhoneBookEntry {
     constructor(params) {
+        this.id = params.id;
         this.name = params.name;
         this.dateOfBirth = params.dateOfBirth;
         this.mobileNumber = params.mobileNumber;
+        this.userId = params.userId;
     }
 
     static create(params) {
